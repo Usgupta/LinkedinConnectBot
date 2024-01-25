c0c009ddc8a7be9871a9fa8eb015fd261336329e
Umang Gupta <umanggupta1103@gmail.com>
2024-01-26 02:01:35 +0800

    connect bot changed for sending connection req for jobs

--- MyConnectmenu.py
+++ MyConnectmenu.py
@@ -3,12 +3,39 @@ import undetected_chromedriver as uc
 from undetected_chromedriver.options import ChromeOptions
 import os
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+
+# driver = uc.Chrome()
+
+def set_value_with_event(element, value, driver):
+    # Click to focus
+    action = ActionChains(driver)
+    action.move_to_element(element).click().perform()
+    
+    # Clear the existing value
+    driver.execute_script("arguments[0].value = '';", element)
+    
+    # Use JavaScript to simulate human typing
+    driver.execute_script("""
+    var setValue = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, 'value').set;
+    var element = arguments[0];
+    var value = arguments[1];
+    
+    setValue.call(element, value);
+    
+    var event = new Event('input', { bubbles: true });
+    element.dispatchEvent(event);
+    """, element, value)
 
 def main():
     Selection = int(input('(1) Send Connection Requests \n(2) Withdraw all pending connections\nWhich would you like to do: '))
-    LoginUser = input('\nEnter your LinkedIn email: ')
-    LoginPass = input('\nEnter your LinkedIn Password: ')
+    # LoginUser = input('\nEnter your LinkedIn email: ')
+    # LoginPass = input('\nEnter your LinkedIn Password: ')
+
+    LoginUser = "umanggupta1103@gmail.com"
+    LoginPass = "8sPK7Zx5M*Hdt8D"
+
 
     print('\nSigning in... (Takes about 10 seconds)')
 
@@ -17,7 +44,7 @@ def main():
     myoptions = ChromeOptions()
     myoptions.add_argument("--headless")
     driver = uc.Chrome(options=myoptions)
-    #driver = uc.Chrome
+    # driver = uc.Chrome()
     
     # Opening LinkedIn Signinpage
     urltoSignInPage = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
@@ -42,31 +69,52 @@ def main():
         chose_withdraw(driver)
 
 def chose_Connect(driver):
-    Keywords = []
-    KeywordNum = int(input('How many keywords would you like to use: '))
 
-    for x in range(KeywordNum):
-        Keywords.append(input(f'Enter Keyword {x+1}: '))
+    JobLink = input('\nEnter your job link: ')
+
+    # Company = input('\nEnter company name: ')
+
+    query = input('\nEnter query : ')
+
+    ConnectionMessage = f"Hi,I am Umang, I found an opening at which fits my interests and skillsets. Would love if you can refer me!\nJD: {JobLink}\nMy portfolio: https://umangpt.com/"
+    # ConnectionMessage = f"Hi,I am Umang, I have applied at Google through a referral for a SWE position , would love to connect and share my portfolio! \nJD: {JobLink}\nMy portfolio: https://umangpt.com/"
+
+    print('\n Your message is: \n', ConnectionMessage)
+
+    if(len(ConnectionMessage)>299):
+        print('too long: ', len(ConnectionMessage))
+        return
+
+
+    # Keywords = []
+
+    # KeywordNum = int(input('How many keywords would you like to use: '))
+
+    # for x in range(KeywordNum):
+    #     Keywords.append(input(f'Enter Keyword {x+1}: '))
         
 
     maxConnect = int(input('\nHow many connection requests would you like to send? (Stay below 50 to be safe): '))
     os.system('cls||clear')
-    Keywords = [w.replace(" ", "%20") for w in Keywords]
-    Keywords = str.join("%20", Keywords)
+    # Keywords = [w.replace(" ", "%20") for w in Keywords]
+    # Keywords = str.join("%20", Keywords)
 
     
     i = 0
     # if program is crashing, increment K variable below by 5
-    k = 25
+    k = 1
+
     print("\nBeginning connection request process...\nThere is a delay between requests intentionally to bypass bot detections")
     while i < maxConnect:
         try:
             # Construct the URL for the search results page
-            urllink = f"https://www.linkedin.com/search/results/people/?&keywords={Keywords}&network=%5B%22S%22%2C%22O%22%5D&origin=SWITCH_SEARCH_VERTICAL&page={k}&sid=aiC&spellCorrectionEnabled=true"
-            
+            # urllink = f"https://www.linkedin.com/search/results/people/?&keywords={Keywords}&network=%5B%22S%22%2C%22O%22%5D&origin=SWITCH_SEARCH_VERTICAL&page={k}&sid=aiC&spellCorrectionEnabled=true"
+            # urllink = f"https://www.linkedin.com/search/results/people/?currentCompany=%5B%22162479%22%5D&origin=FACETED_SEARCH&page={k}"
             # Load the search results page and wait for it to load
+            # urllink = query
+            urllink = query+f'&page={k}'
             driver.get(urllink)
-            time.sleep(randint(4, 15))
+            time.sleep(randint(4, 6))
 
             # Find all the Connect buttons on the page
             all_buttons = driver.find_elements(By.TAG_NAME, "button")
@@ -75,14 +123,32 @@ def chose_Connect(driver):
             # Loop through all the Connect buttons and send connection requests
             for btn in connect_buttons:
                 driver.execute_script("arguments[0].click();", btn)
-                name = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[2]/p/span/strong").text
-                print(f"Sending connection request to {name}")
-                time.sleep(randint(4, 15))
-                send = driver.find_element(By.XPATH, "//button[@aria-label='Send now']")
-                driver.execute_script("arguments[0].click();", send)
-                close = driver.find_element(By.XPATH, "//button[@aria-label='Dismiss']")
-                driver.execute_script("arguments[0].click();", close)
-                time.sleep(2)
+                try: 
+                    name = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[2]/p/span/strong").text
+                    print(f"Sending connection request to {name}")
+                    time.sleep(randint(4, 5))
+                    add_note = driver.find_element(By.XPATH, "//button[@aria-label='Add a note']")
+                    driver.execute_script("arguments[0].click();", add_note)
+
+                    print(f"Adding a note to {name}")
+                    time.sleep(randint(4, 5))
+
+                    text_field = driver.find_element(By.XPATH, '//textarea[@id="custom-message"]')
+                    print(f"Finding text field for {name}")
+                    time.sleep(randint(2, 3))
+
+                    set_value_with_event(text_field, ConnectionMessage, driver)
+                    print(f"Setting text field for {name}")
+                    time.sleep(randint(4, 4))
+                    send = driver.find_element(By.XPATH, "//button[@aria-label='Send now']")
+                    driver.execute_script("arguments[0].click();", send)
+                    print(f"sending request {name}")
+
+                    # close = driver.find_element(By.XPATH, "//button[@aria-label='Dismiss']")
+                    # driver.execute_script("arguments[0].click();", close)
+                    time.sleep(2)
+                except Exception as e:
+                    print(f"Inner error occurred: {str(e)}")
 
             # If there are no more Connect buttons on the page, go to the next page
             if len(connect_buttons) == 0:
@@ -93,8 +159,8 @@ def chose_Connect(driver):
                 k += 1
 
             # Print the total number of connection requests sent so far
-            print(f"Connection Invitations sent = {i}")
-            time.sleep(randint(4, 15))
+            print(f"Connection Invitations sent = {i}, page = {k}")
+            time.sleep(randint(4, 6))
 
             # Exit the loop if the maximum number of connections has been reached
             if i >= maxConnect:
